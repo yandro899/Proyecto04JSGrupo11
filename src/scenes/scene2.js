6cd68028c3cc4bf94a10937130f2cefd1e5009e1
Carrasco Candela E R <candenwnh@gmail.com>
2023-10-12 19:26:48 -0300

    ahora hay explociones en cada colicion con balas

--- src/scenes/scene2.js
+++ src/scenes/scene2.js
@@ -15,6 +15,7 @@ export class Scene2 extends Phaser.Scene{
         
         this.load.spritesheet('nave2','../public/img/nave2.png',{ frameWidth: 71, frameHeight: 62 });
         this.load.spritesheet('enemy2','../public/img/enemy2.png',{ frameWidth: 70, frameHeight: 62 });
+        this.load.spritesheet('explosion','../public/img/explosion.png',{ frameWidth: 30, frameHeight: 30 });
         this.load.image('boss','../public/img/naveGrande.png');
         this.load.image('noche', '../../public/img/nigth.png');
         this.load.image('fire', '../../public/img/yellow.png');
@@ -128,18 +129,20 @@ export class Scene2 extends Phaser.Scene{
         this.vidaPlayerText = this.add.text(500, 16, 'Vida Jugador: 100 ', { fontSize: '50px', fill: '#fff' });
         this.puntosText = this.add.text(16, 0, 'Puntos: 0', { fontSize: '20px', fill: '#fff' });
 
+        this.anims.create({
+            key: 'explotar',
+            frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 5 }),
+            frameRate: 10,
+        });
+
         //detecta las coliciones entre las balas y enemigos
         this.physics.add.overlap(this.bullets, this.enemies2, (bala, enemigo)=>{
-            this.add.particles(bala.x, bala.y, 'fire', {
-                speed: 150,
-                scale: { start: 1, end: 1.5 },
-                blendMode: 'ADD',
-                duration: 200
-            });
             bala.destroy();
             enemigo.destroy();
             this.puntos += 10;
             this.puntosText.setText('Puntos: ' + this.puntos);
+            this.exp = this.add.sprite(bala.x + 30, bala.y, 'explosion');
+            this.exp.anims.play('explotar', true);
 
         }, null, this);
         this.physics.add.overlap(this.superBullets, this.enemies2, (bala, enemigo)=>{
@@ -147,6 +150,8 @@ export class Scene2 extends Phaser.Scene{
             this.puntos += 10*Math.pow(2,this.countSuperHits);
             this.countSuperHits++;
             this.puntosText.setText('Puntos: ' + this.puntos);
+            this.exp = this.add.sprite(bala.x + 30, bala.y, 'explosion');
+            this.exp.anims.play('explotar', true);
         }, null, this);
 
         
@@ -159,7 +164,8 @@ export class Scene2 extends Phaser.Scene{
             this.vidaText.setText('Vida Jefe: ' + this.vidaBoss);
             this.puntos += 10;
             this.puntosText.setText('Puntos: ' + this.puntos);
-            
+            this.exp = this.add.sprite(bala.x + 30, bala.y, 'explosion');
+            this.exp.anims.play('explotar', true);
         }, null, this); 
         this.physics.add.overlap(this.superBullets, this.boss, (jefe, bala)=>{
             if (!this.bossStart)
@@ -170,6 +176,8 @@ export class Scene2 extends Phaser.Scene{
             this.puntos += 10*Math.pow(2,this.countSuperHits);
             this.countSuperHits = 0;
             this.puntosText.setText('Puntos: ' + this.puntos);
+            this.exp = this.add.sprite(bala.x + 30, bala.y, 'explosion');
+            this.exp.anims.play('explotar', true);
         }, null, this); 
 
         // Colisiones del jugador con los enemigos basicos
